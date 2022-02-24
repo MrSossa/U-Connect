957088e0b22325fccc2d2bde5558f59bd3cab304
jacevar <jac22evar@gmail.com>
2022-02-24 01:15:05 -0500

    interface update

--- UConnectProject/carpooling/admin.py
+++ UConnectProject/carpooling/admin.py
@@ -1,9 +1,1 @@
 from django.contrib import admin
-from django_google_maps import widgets as map_widgets
-from django_google_maps import fields as map_fields
-
-class RentalAdmin(admin.ModelAdmin):
-    formfield_overrides = {
-        map_fields.AddressField: {'widget': map_widgets.GoogleMapsAddressWidget},
-    }
-# Register your models here.
