530e17d66d1f0ba9a8d483c8decb993ab0d6f3d2
jacevar <jac22evar@gmail.com>
2022-03-16 11:46:45 -0500

    update

--- UConnectProject/UConnect/urls.py
+++ UConnectProject/UConnect/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main import views as mainViews
+from account import views as accViews
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainViews.home),
+    path('about/', mainViews.about),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- UConnectProject/main/models.py
+++ UConnectProject/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    email = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='movie/images/')
+    url = models.URLField(blank=True)
+
+class Car(models.Model):
+    Brand = models.CharField(max_length=100)
+    Model = models.CharField(max_length=100)
+    Placa = models.CharField(max_length=6)
+    color = models.CharField(max_length=20)
+    url = models.URLField(blank=True)
